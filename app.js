9e9cace3324b802cc49326f5daa127f8e30705e7
dorswisa <swisalem@gmail.com>
2021-10-08 01:15:31 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+
+var http = require('http');
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.locals.pretty = true;
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/app/server/views');
+app.set('view engine', 'pug');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(require('stylus').middleware({ src: __dirname + '/app/public' }));
+app.use(express.static(__dirname + '/app/public'));
+
+app.get('/', function(req, res){
+	res.render('home');
+});
+
+app.get('*', function(req, res) { res.render('404', { title: 'Page Not Found'}); });
+
+http.createServer(app).listen(app.get('port'), function(){
+	console.log('Express server listening on port ' + app.get('port'));
+});
+
